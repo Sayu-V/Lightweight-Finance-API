82b6b317d490e4472a5cf8635842a36aa48d3c1f
Sayooj Vandichal <sayujrsayu@gmail.com>
2026-03-19 19:52:28 +0530

    Update models.py

--- models.py
+++ models.py
@@ -3,3 +3,8 @@ from pydantic import BaseModel, Field
 class Income(BaseModel):
     amount: float = Field(..., gt=0)
     source: str = Field(..., min_length=1, max_length=50)
+
+# ✅ NEW MODEL
+class Expense(BaseModel):
+    amount: float = Field(..., gt=0)
+    category: str = Field(..., min_length=1, max_length=50)
